476af07dd97a3b51255c842e2d6fb59087a086e0
George vila <gvila92@gmail.com>
2023-10-05 16:24:35 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(objects) {
+    const winObject = objects.find(object => object.result === "W");
+    return winObject ? winObject.year : undefined;
+  }
